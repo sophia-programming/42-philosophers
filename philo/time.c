a15b770d48536014c0421f785ea0e4b961a21152
oaoba <error@gmail.com>
2023-08-22 21:53:29 +0900

    {0}この初期化の方法は構造体の全てのメンバを０初期化したい時に有効

--- philo/time.c
+++ philo/time.c
@@ -18,7 +18,7 @@ void	precise_sleep(size_t sleep_time)
 {
 	size_t	end_time;
 
-	end_time = get_time() + sleep_time;
+	end_time = get_time() + sleep_time;//end_timeはsleepが終了する時間
 	while (get_time() < end_time)
 		usleep(100);
 }
